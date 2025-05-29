46dfd0f08b68fc5c28e1bb1cb247da36748848a8
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-29 20:10:22 +0300

    fix: imported lombok

--- src/main/java/gr/aueb/cf/teacherapp/dto/TeacherInsertDTO.java
+++ src/main/java/gr/aueb/cf/teacherapp/dto/TeacherInsertDTO.java
@@ -1,23 +1,31 @@
 package gr.aueb.cf.teacherapp.dto;
 
-
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class TeacherInsertDTO {
 
     @NotNull(message = "Το όνομα δεν μπορεί να είναι κενό")
     @Size(min = 2, message = "Το όνομα πρέπει να περιέχει τουλάχιστον 2 χαρακτήρες.")
-    private String firstName;
+    private String firstname;
 
     @NotNull(message = "Το επώνυμο δεν μπορεί να είναι κενό")
     @Size(min = 2, message = "Το επώνυμο πρέπει να περιέχει τουλάχιστον 2 χαρακτήρες.")
-    private String lastName;
+    private String lastname;
 
     @Pattern(regexp = "\\d{9,}", message = "Το ΑΦΜ δεν μπορεί να είναι μικρότερο από 9 ψηφία.")
     private String vat;
 
     @NotNull (message = "Η περιοχή δεν μπορεί να είναι κενή")
     private Long regionId;
+
 }
