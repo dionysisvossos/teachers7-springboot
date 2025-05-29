f065fa06e97f952c2ecde90a462a3600f0206741
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-29 20:09:10 +0300

    fix: typos corrected

--- src/main/java/gr/aueb/cf/teacherapp/dto/TeacherReadOnlyDTO.java
+++ src/main/java/gr/aueb/cf/teacherapp/dto/TeacherReadOnlyDTO.java
@@ -17,8 +17,8 @@ public class TeacherReadOnlyDTO {
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
     private String uuid;
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
     private String vat;
     private String region;
 }
