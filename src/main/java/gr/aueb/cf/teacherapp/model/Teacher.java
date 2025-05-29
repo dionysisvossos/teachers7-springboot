732e153d412bbdc87f36a519788a615850a460fd
Dionysis.Vossos <dionysis.vossos@windowslive.com>
2025-05-29 18:30:06 +0300

    initial commit

---
+++ src/main/java/gr/aueb/cf/teacherapp/model/Teacher.java
@@ -0,0 +1,41 @@
+package gr.aueb.cf.teacherapp.model;
+
+import gr.aueb.cf.teacherapp.model.static_data.Region;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
+
+    @Column(unique = true)
+    private String vat;
+
+    private String firstName;
+    private String lastName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    @PrePersist
+    public void initiateUuid() {
+        if (uuid == null)  uuid = UUID.randomUUID().toString();
+    }
+
+}
